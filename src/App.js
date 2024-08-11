aa0893f4ad8a1a2aaf391357a4017e9f287195a0
pmounika4703 <145027471+pmounika4703@users.noreply.github.com>
2024-08-11 10:59:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Swicth, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Swicth, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Users from './components/Users';
 import Posts from './components/Posts';
